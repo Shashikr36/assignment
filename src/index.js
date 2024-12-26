88b52611799a2c28e74ba0fc70d33ca86262351e
Shashikr36 <shashikrpatwa36@gmail.com>
2024-12-26 17:32:23 +0530

    Updated src/index.js

--- src/index.js
+++ src/index.js
@@ -15,12 +15,3 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
-
-//echo "# assignment" >> README.md
-//git init
-//git add README.md
-//git commit -m "first commit"
-//git branch -M main
-//git remote add origin https://github.com/Shashikr36/assignment.git
-//git push -u origin main
